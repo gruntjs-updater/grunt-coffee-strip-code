52d8f32ecda4ae79938ae7daaf5d2bd0d0c621c7
Maciej Sypien <maciej.sypien@itelo.com>
2014-09-30 16:31:53 +0200

    Changed files to proper extension

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,34 +50,34 @@ module.exports = function(grunt) {
     // Configuration to be run (and then tested).
     coffee_strip_code: {
       default_options: {
-        src: 'tmp/default_options.js',
+        src: 'tmp/default_options.coffee',
       },
       start_end_options: {
         options: {
           start_comment_tag: '{test}',
           end_comment_tag: '{/test}',
         },
-        src: 'tmp/start_end_options.js',
+        src: 'tmp/start_end_options.coffee',
       },
       pattern_options: {
         options: {
           pattern: / *console\.log\(['"a-z]+\)\n?/g
         },
-        src: 'tmp/pattern_options.js',
+        src: 'tmp/pattern_options.coffee',
       },
       dest_specified: {
         files: [
-          {src: 'tmp/dest_specified.js', dest: 'tmp/dest_specified2.js'},
+          {src: 'tmp/dest_specified.coffee', dest: 'tmp/dest_specified2.coffee'},
         ]
       },
       multiple_files: {
-        src: ['tmp/multiple_files*.js', 'tmp/another_multiple_file.js']
+        src: ['tmp/multiple_files*.coffee', 'tmp/another_multiple_file.coffee']
       },
     },
 
     // make unit tests
     nodeunit: {
-      tests: ['tests/*_test.js']
+      tests: ['tests/*_test.coffee']
     },
 
   })
